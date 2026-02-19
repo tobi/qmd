630b149fdf6689609a73a89a0571620445598a18
ruiwarn <ruiwarn@gmail.com>
2026-02-19 16:21:42 +0800

    refactor: simplify setConfigIndexName (remove path normalization)

--- src/collections.ts
+++ src/collections.ts
@@ -75,16 +75,7 @@ let currentIndexName: string = "index";
  * Config file will be ~/.config/qmd/{indexName}.yml
  */
 export function setConfigIndexName(name: string): void {
-  // Resolve relative paths to absolute paths and sanitize for use as filename
-  if (name.includes('/')) {
-    const { resolve } = require('path');
-    const { cwd } = require('process');
-    const absolutePath = resolve(cwd(), name);
-    // Replace path separators with underscores to create a valid filename
-    currentIndexName = absolutePath.replace(/\//g, '_').replace(/^_/, '');
-  } else {
-    currentIndexName = name;
-  }
+  currentIndexName = name;
 }
 
 function getConfigDir(): string {
